ec9907eeec930ef57152ca46b73a7b27e7ee0247
Nayan <geeknayan@gmail.com>
2026-06-03 11:37:32 +0530

    kjsdjka

--- internal/09-log-publisher/02-variant/01-LogPublishingManager.h
+++ internal/09-log-publisher/02-variant/01-LogPublishingManager.h
@@ -1,6 +1,9 @@
 #ifndef LOGPUBLISHINGMANAGER_INTERNAL_H
 #define LOGPUBLISHINGMANAGER_INTERNAL_H
 
+#include <ArduinoJson.h>
+#include <ctime>
+
 #include <StandardDefines.h>
 #include "../01-interface/01-ILogPublishingManager.h"
 #include "communication/IServerProvider.h"
@@ -43,37 +46,33 @@ class LogPublishingManager : public ILogPublishingManager {
     }
 
     Private StdString GetLogsJson(const StdMap<ULongLong, StdString>& logs) {
-        StdString jsonLogs = "[";
-
-        int logCount = logs.size();
-        int index = 0;
-
-        
+        DynamicJsonDocument doc(4096);
+        JsonArray arr = doc.to<JsonArray>();
+    
         for (const auto& log : logs) {
-
-            time_t nowSec = (time_t)log.first;
+            // Convert back to seconds
+            time_t nowSec = static_cast<time_t>(log.first / 1000ULL);
+    
             char timeBuf[24];
-            if (nowSec != (time_t)-1 && nowSec > 0) {
+            if (nowSec > 0) {
                 struct tm* t = localtime(&nowSec);
-                if (t && strftime(timeBuf, sizeof(timeBuf), "%Y-%m-%d %H:%M:%S", t) > 0) { /* ok */ }
-                else snprintf(timeBuf, sizeof(timeBuf), "(time?)");
+                if (t && strftime(timeBuf, sizeof(timeBuf), "%Y-%m-%d %H:%M:%S", t) > 0) {
+                    // formatted successfully
+                } else {
+                    snprintf(timeBuf, sizeof(timeBuf), "(time?)");
+                }
             } else {
                 snprintf(timeBuf, sizeof(timeBuf), "(no time)");
             }
-            StdString timeStr = timeBuf;
     
-
-
-            jsonLogs += "{\"" + timeStr + " \": \"" + log.second + "\"}";
-            index++;
-            if(index < logCount) {
-                jsonLogs += ",";
-            }
+            JsonObject obj = arr.createNestedObject();
+            obj[timeBuf] = log.second;
         }
-
-        jsonLogs += "]";
-        return jsonLogs;
-    }
+    
+        StdString output;
+        serializeJson(arr, output);
+        return output;
+    }    
 };
 
 
