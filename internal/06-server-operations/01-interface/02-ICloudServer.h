f67765fb6c3de377325ffb8d7ef08425f83e4f97
Nayan <geeknayan@gmail.com>
2026-06-11 19:09:35 +0530

    kjdjkas

--- internal/06-server-operations/01-interface/02-ICloudServer.h
+++ internal/06-server-operations/01-interface/02-ICloudServer.h
@@ -9,6 +9,10 @@ DefineStandardPointers(ICloudServer)
 class ICloudServer : public IServer {
     Public Virtual ~ICloudServer() = default;
     Public Virtual Bool PublishLogs(CStdString logs) = 0;
+
+    Public Virtual Bool PublishSecondPulse(CStdString payload) = 0;
+
+    Public Virtual Bool PublishThirtyMinuteBucket(CStdString payload) = 0;
 };
 
 #endif // ICLUDSERVER_EXPORT_H
--- internal/06-server-operations/02-variant/02-CloudServer.h
+++ internal/06-server-operations/02-variant/02-CloudServer.h
@@ -2,9 +2,11 @@
 #define CLOUDSERVER_INTERNAL_H
 
 #include <StandardDefines.h>
+#include <ctime>
 #include "communication/ICloudServer.h"
 #include "server/IMqttClient.h"
 #include "server/IDeviceService.h"
+#include "service/IConnectionDetailsProvider.h"
 
 /* @Component */
 class CloudServer final : public ICloudServer {
@@ -15,34 +17,34 @@ class CloudServer final : public ICloudServer {
 
     /* @Autowired */
     Private IMqttClientPtr mqttClient;
-    
+
     /* @Autowired */
     Private IDeviceServicePtr deviceService;
+
+    /* @Autowired */
+    Private IConnectionDetailsProviderPtr connectionDetailsProvider;
 
     Private Optional<DeviceIdentityProfileData> deviceIdentityProfile;
 
     Public Bool Start() override {
-        // Do nothing as MqttClient will be managed by MqttClientManager
         deviceIdentityProfile = deviceService->GetDeviceIdentityProfile();
-        return mqttClient->IsConnected();        
+        return mqttClient->IsConnected();
     }
 
     Public Void Stop() override {
-        // Do nothing as MqttClient will be managed by MqttClientManager
     }
 
     Public Bool Restart() override {
-        // Do nothing as MqttClient will be managed by MqttClientManager
         deviceIdentityProfile = deviceService->GetDeviceIdentityProfile();
         return false;
     }
-    
+
     Public Bool IsRunning() const override {
         return mqttClient->IsConnected();
     }
-    
+
     Public IHttpRequestPtr ReceiveMessage() override {
-        if(!deviceIdentityProfile.has_value()) {
+        if (!deviceIdentityProfile.has_value()) {
             return nullptr;
         }
 
@@ -56,7 +58,7 @@ class CloudServer final : public ICloudServer {
     }
 
     Public Bool SendMessage(CStdString& requestId, CStdString& message) override {
-        if(!deviceIdentityProfile.has_value()) {
+        if (!deviceIdentityProfile.has_value()) {
             return false;
         }
 
@@ -69,12 +71,46 @@ class CloudServer final : public ICloudServer {
     }
 
     Public Bool PublishLogs(CStdString logs) override {
+        if (!deviceIdentityProfile.has_value()) {
+            return false;
+        }
         CStdString path = deviceIdentityProfile.value().publishTopics.logsTopic;
         MqttMessage mqttMessage = {
             .guid = "logs",
             .payload = logs,
         };
         return mqttClient->QueueMessageToSend(path, mqttMessage);
+    }
+
+    Public Bool PublishSecondPulse(CStdString payload) override {
+        return PublishWaterTelemetry("telemetry/second", "second-pulse", payload);
+    }
+
+    Public Bool PublishThirtyMinuteBucket(CStdString payload) override {
+        return PublishWaterTelemetry("telemetry/bucket/30m", "bucket-30m", payload);
+    }
+
+    Private Bool PublishWaterTelemetry(CStdString suffix, CStdString guidPrefix, CStdString payload) {
+        if (!mqttClient->IsConnected()) {
+            return false;
+        }
+        CStdString tenantId = connectionDetailsProvider->GetTenantId();
+        CStdString thingName = connectionDetailsProvider->GetThingName();
+        if (tenantId.empty() || thingName.empty()) {
+            return false;
+        }
+        CStdString topic = BuildWaterTopic(suffix);
+        CStdString guid = guidPrefix + "-" + std::to_string(static_cast<unsigned long long>(time(nullptr)));
+        MqttMessage mqttMessage = {
+            .guid = guid,
+            .payload = payload,
+        };
+        return mqttClient->QueueMessageToSend(topic, mqttMessage);
+    }
+
+    Private CStdString BuildWaterTopic(CStdString suffix) const {
+        return "vswitch/water/" + connectionDetailsProvider->GetTenantId() + "/"
+             + connectionDetailsProvider->GetThingName() + "/" + suffix;
     }
 };
 #endif // CLOUDSERVER_INTERNAL_H
